bc468bd28714ac41b1f83d9a9610c9cbcaa82251
jcuetos97 <105081149+jcuetos97@users.noreply.github.com>
2022-09-20 18:00:12 -0500

    package.json, dependencies installed

--- server.js
+++ server.js
@@ -7,7 +7,7 @@ const routes = require('./controllers/');
 
 // Setting up PORT and app
 const app = express();
-const PORT = process.env.PORT || 3001;
+const PORT = process.env.PORT || 3005;
 
 const sequelize = require('./config/connection');
 const SequelizeStore = require('connect-session-sequelize')(session.Store);
