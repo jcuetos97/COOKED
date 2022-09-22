941b2877a2c0c894217a636060559eeabfdc0ed7
jcuetos97 <105081149+jcuetos97@users.noreply.github.com>
2022-09-22 12:58:31 -0500

    newPost.handlebars

--- controllers/api/postRoutes.js
+++ controllers/api/postRoutes.js
@@ -2,7 +2,51 @@ const router = require('express').Router();
 const { Post } = require('../../models');
 const withAuth = require('../../utils/auth');
 
-
+router.post('/new', withAuth, async (req, res) => {
+    try {
+      const newPost = await Post.create({
+        title: req.body.title,
+        body: req.body.body,
+        user_id: req.session.user_id
+        });
+        
+        res.json({ message: 'Your post has been published successfully!' });
+      
+    } catch (err) {
+      res.status(400).json(err);
+    }
+  });
+  
+  router.put("/:id", withAuth, async (req, res) => {
+    try {
+      Post.update(req.body,{
+        where: {
+          id: req.params.id
+        }
+      });
+  
+      res.status(200).end();
+       
+    } catch (err) {
+      res.status(400).json(err);
+    }
+  });
+  
+  router.delete("/:id", withAuth, async (req, res) => {
+    try {
+      Post.destroy({
+        where: {
+          id: req.params.id
+        }
+      });
+  
+      res.status(200).end();
+       
+    } catch (err) {
+      res.status(400).json(err);
+    }
+  });
+    
   
 
 module.exports = router; 
