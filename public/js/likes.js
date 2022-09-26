6a892433df9bf6c604ac9da77be77ea8a67198bd
jcuetos97 <105081149+jcuetos97@users.noreply.github.com>
2022-09-26 11:23:40 -0500

    like bttn update

--- public/js/likes.js
+++ public/js/likes.js
@@ -1,4 +1,19 @@
-const button = document.querySelector('.btn')
-button.addEventListener('click', () => {
-    button.classList.toggle('liked')
-})
+const likebtn = document.getElementById('heart-icon')
+let emptyheart= true;
+
+likebtn.addEventListener('click', () => {
+    if (emptyheart) {
+        likebtn.classList.toggle('fa-regular');
+        likebtn.classList.add('fa-solid');
+        emptyheart = false;
+    } else {
+        likebtn.classList.toggle('fa-solid')
+        likebtn.classList.add('fa-regular')
+        emptyheart = true;
+    }
+}) 
+
+
+
+
+
